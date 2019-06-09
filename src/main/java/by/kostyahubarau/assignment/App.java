05d165a47acab9bd666a989d3508327bbff0a765
Kanstantsin Hubarau <kostyahubarau@gmail.com>
2019-06-09 17:40:35 +0200

    Create project stub from maven-archetype-quickstart:1.4.

---
+++ src/main/java/by/kostyahubarau/assignment/App.java
@@ -0,0 +1,13 @@
+package by.kostyahubarau.assignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
